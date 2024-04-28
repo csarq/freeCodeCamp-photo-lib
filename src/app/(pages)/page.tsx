5a875166cd48e81d4a1b4cda216e5ff595e77d2f
teonkajkafon <catherinesarquis@gmail.com>
2024-04-28 11:39:05 +0100

    set up cloudinary

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,4 +1,11 @@
-import MediaGallery from '@/components/MediaGallery';
+import MediaGallery from "@/components/MediaGallery";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export default async function Home() {
   return (
@@ -6,10 +13,10 @@ export default async function Home() {
       <MediaGallery
         resources={[
           {
-            id: 'my-image'
-          }
+            id: "my-image",
+          },
         ]}
       />
     </div>
-  )
+  );
 }
