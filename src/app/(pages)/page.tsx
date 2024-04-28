d32ab34dfec6db9871a4a9ea4b6faefd14362462
teonkajkafon <catherinesarquis@gmail.com>
2024-04-28 12:44:09 +0100

    list photos from Cloudinary

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -8,15 +8,10 @@ cloudinary.config({
 });
 
 export default async function Home() {
+  const { resources } = await cloudinary.api.resources();
   return (
     <div className="h-full mt-6">
-      <MediaGallery
-        resources={[
-          {
-            id: "my-image",
-          },
-        ]}
-      />
+      <MediaGallery resources={resources} />
     </div>
   );
 }
