435583f28fe97e322658ef5aa6ab06e5228384f7
teonkajkafon <catherinesarquis@gmail.com>
2024-04-28 13:46:48 +0100

    optimise images

--- src/components/MediaGallery/MediaGallery.tsx
+++ src/components/MediaGallery/MediaGallery.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import { Plus, X, Save } from "lucide-react";
+import { CldImage } from "next-cloudinary";
 
 import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
@@ -163,10 +164,11 @@ const MediaGallery = ({ resources }: MediaGalleryProps) => {
                         }`}
                         href="#"
                       >
-                        <img
+                        <CldImage
                           width={resource.width}
                           height={resource.height}
-                          src={resource.secure_url}
+                          src={resource.public_id}
+                          sizes="(min-width: 768px) 33vw, (min-width: 1024px) 25vw, (min-width: 1280px) 20vw"
                           alt=""
                         />
                       </Link>
